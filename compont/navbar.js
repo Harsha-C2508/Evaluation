42440b43820fd81a8ed67b0c1d82580152315811
Harsha-C2508 <harshac2508@gmail.com>
2022-05-01 21:37:22 +0530

    import export

---
+++ compont/navbar.js
@@ -0,0 +1,14 @@
+let navbar = ()=>{
+    return `<div id="searchBar">
+            <input type="text" id="query">
+        </div>
+        <div id="cate">
+            <h3>Nature</h3>
+            <h3>Cars</h3>
+            <h3>Music</h3>
+            <h3>Cartoon</h3>
+            <h3>Technologies</h3>
+            <h3>Marvels</h3>
+        </div>`;
+}
+export {navbar};
---
+++ script/index.js
@@ -0,0 +1,4 @@
+// https://api.unsplash.com/search/photos/?query=${value}&per_page=20&client_id=q3kZK1A3_J0wHHKXMJ1HQmSxlmBo80CxYxIT95aErF4
+
+import { navbar } from "../compont/navbar";
+document.getElementById(navbar).innerHTML = navbar();
